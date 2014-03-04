83db6471c45c8ce320d55e2b152952248be70a6a
igor <igor@d1stkfactory>
2014-03-04 10:00:46 +0000

    Example lib1

---
+++ summBN.hpp
@@ -0,0 +1,13 @@
+namespace Bnum
+{
+	class Integ
+	{
+		public:
+			unsigned long long a, b;			//От 0 до 18 446 744 073 709 551 615 
+		private:
+			unsigned long long summ(a,b)
+			{
+				return a+b;
+			}
+	};
+}
