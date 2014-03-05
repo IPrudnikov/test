0195afdcb439a8693c69f693cd63e06370e4fdae
IPrudnikov <kot220487@bk.ru>
2014-03-05 12:45:38 +0000

    + cin

--- testproj.cpp
+++ testproj.cpp
@@ -5,8 +5,13 @@
 int main()
 {
 	unsigned long long a, b;			//От 0 до 18 446 744 073 709 551 615 
-	a = 18446744073709551613U;
-	b = 1;
+	
+	std::cout << "Value 1: " << std::endl;
+	std::cin >> a;
+	std::cout << "Value 2: " << std::endl;
+	std::cin >> b;
+	//a = 18446744073709551613U;
+	//b = 1;
 	std::cout << Integ::summ(a,b) << std::endl << sizeof(long long) << std::endl;
 	
 	return 0;
